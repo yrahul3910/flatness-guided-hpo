deb77560bf46f58042ded5967500ceddde989eda
Rahul Yedida <rahul.yedida@pm.me>
2024-01-18 00:01:28 -0500

    Added more results

--- hpobench/util/eval.py
+++ hpobench/util/eval.py
@@ -12,8 +12,8 @@ def eval(config: Config, data: Dataset, *args, **kwargs) -> List[float]:
     model = get_model(config, data)
 
     try:
-        early_stop = EarlyStopping(monitor='loss', patience=5)
-        model.fit(data.x_train, data.y_train, batch_size=config.batch_size, epochs=100, callbacks=[early_stop])
+        early_stop = EarlyStopping(monitor='loss', patience=5, min_delta=1e-3)
+        model.fit(data.x_train, data.y_train, batch_size=config.batch_size, epochs=200, callbacks=[early_stop])
         score = model.evaluate(data.x_test, data.y_test, verbose=0)
         
         print(f'Score: {score}')
